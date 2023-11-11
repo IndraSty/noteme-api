2ef345ee2bc554678dfbbe6fe3775b3391127a59
Indoradesu <indrastyawan0925@gmail.com>
2023-11-11 23:33:01 +0700

    update

--- src/routes/public-routes.js
+++ src/routes/public-routes.js
@@ -3,6 +3,9 @@ import userController from "../controller/user-controller.js";
 import { refreshToken } from "../controller/refresh-token.js";
 
 export const publicRouter = express.Router();
+publicRouter.get('/', (req, res) => {
+    res.send('Server running!')
+  })
 publicRouter.post('/api/users', userController.register);
 publicRouter.post('/api/users/login', userController.login)
 publicRouter.get('/api/token', refreshToken)
